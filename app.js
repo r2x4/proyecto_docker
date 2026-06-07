0a73f27e9e8d9c248c1a504f6b60a1f119aaa0bd
Rodrigo Rocha <rrs230780@gmail.com>
2026-06-07 14:39:13 -0500

    Corregir host de conexión MySQL a mysql-db y agregar connect()

--- app.js
+++ app.js
@@ -4,25 +4,28 @@ const mysql = require('mysql2');
 const app = express();
 
 const connection = mysql.createConnection({
-    host: 'host.docker.internal',
+    host: 'mysql-db',
     user: 'root',
     password: 'Dragon2307*',
     database: 'venta_jugos'
 });
 
+connection.connect((err) => {
+    if (err) {
+        console.error('Error conectando a MySQL:', err);
+        return;
+    }
+    console.log('Conectado a MySQL correctamente');
+});
+
 app.get('/', (req, res) => {
-
     connection.query('SELECT * FROM tb_cliente', (err, results) => {
-
         if (err) {
             console.error(err);
             return res.send('Error conectando a MySQL');
         }
-
         res.json(results);
-
     });
-
 });
 
 app.listen(3000, () => {
