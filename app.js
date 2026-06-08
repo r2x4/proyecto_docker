427ae2f0cf906a31ba7a226cd38727ccd9938a1b
Rodrigo Rocha <rrs230780@gmail.com>
2026-06-08 13:10:01 -0500

    json2

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+
+const connection = mysql.createConnection({
+    host: 'host.docker.internal',
+    user: 'root',
+    password: 'Dragon2307*',
+    database: 'venta_jugos'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error conectando a MySQL:', err);
+        return;
+    }
+    console.log('Conectado a MySQL correctamente');
+});
+
 app.get('/', (req, res) => {
     connection.query('SELECT * FROM tb_cliente', (err, results) => {
 
@@ -75,4 +95,8 @@ app.get('/', (req, res) => {
 
         res.send(html);
     });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en puerto 3000');
 });
